ffa22dc866b17b5a86648b967bf98cce3d31a4c2
Viktor Holk <viktorholk@gmail.com>
2021-01-09 00:30:50 +0100

    User view set

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
-# Create your views here.
+from . import seriallizers
+from . import models
+from . import permissions
+
+class UserViewSet(viewsets.ModelViewSet):
+    # Handles creating and updating users
+
+    serializer_class        = seriallizers.UserSeriallizer
+    queryset                = models.User.objects.all()
+    authentication_classes  = ( TokenAuthentication, )
+    permission_classes      = ( permissions.UpdateProfile, )
+    filter_backends         = ( filters.SearchFilter, )
+    search_fields           = ( 'email', 'username', )
